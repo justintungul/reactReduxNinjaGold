c19bbe82b3b23aa649dcd9af6dd8a04890334bee
Justin Tungul <justin.tungul@gmail.com>
2019-07-03 22:37:20 -0700

    Capitalize activity name

--- src/components/presentation/ActivityCard/ActivityCard.js
+++ src/components/presentation/ActivityCard/ActivityCard.js
@@ -2,6 +2,10 @@ import React from 'react';
 
 import './ActivityCard.css';
 
+const capitalize = (str) => {
+        return str.charAt(0).toUpperCase() + str.slice(1);
+    }
+
 const ActivityCard = ({
     verb,
     location,
@@ -10,7 +14,7 @@ const ActivityCard = ({
     <div className="col-md-3">
         <div className="card mb-5">
             <div className="card-body">
-                <h1 className="display-1">{location}</h1>
+                <h1 className="display-1">{capitalize(location)}</h1>
                 <h1 className="display-4">(Earns 10 - 20 Gold)</h1>
                 <button onClick={onClick} className="btn btn-lg btn-success mt-4">{verb}!</button>
             </div>
